ba6c6357cc25a6a160d6d2899ac10287ad60a004
Alexei <alexei.danilov@kone.com>
2020-01-21 13:19:10 +0200

    - Game_manager class split into h and cpp file - "using namespace" removed

---
+++ Game_Manager.h
@@ -0,0 +1,20 @@
+#include <string>
+
+class Game_Manager
+{
+public:
+ 
+	//Text created by game
+	void Game_Text(std::string Ss);
+    
+	//In game user input function
+	void User_Text(std::string Question, std::string Respons);
+
+	void Delay(float User_Seconds);
+
+	void fake_loading();
+
+private:
+	std::string User_answer;
+
+};
